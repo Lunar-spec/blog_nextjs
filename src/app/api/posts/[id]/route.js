0797d2068a5a811a03ca03de291afa029ba1b2a3
Manjesh Singh <smanjesh1854@gmail.com>
2023-08-30 23:06:21 +0530

    updated server

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -4,12 +4,12 @@ import Post from "@/models/Post";
 
 export const GET = async (request, { params }) => {
     const { id } = params;
-    console.log(id + params)
+    // console.log(id + params)
     // console.log(request)
     try {
         await connect()
         const posts = await Post.findById(id);
-        console.log(posts)
+        // console.log(posts)
         return new NextResponse(JSON.stringify(posts), { status: 200 })
     } catch (error) {
         return new NextResponse('Database Error', { status: 500 })
